d84d191b7ffa026f330cfa10fd335ecda1d76754
ShokhrukhMirzo9449 <muslimboy9449@gmail.com>
2023-09-23 21:22:01 +0500

    first commit

---
+++ src/style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const DivMain=styled.div`
+    display: flex;
+    height: 91px;
+    border: 1px solid black;
+    font-family: Pacifico;
+    justify-content: space-between;
+    padding: 13px 135px;
+    
+`
+export const DivLeft=styled.div`
+    display: flex;
+    flex: 2;
+
+`
+export const DivRight=styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: space-between;
+    padding: 21px 0;
+    font-family: poppins;
+
+`
+export const CartDiv=styled.div`
+    border-left: 1px solid #EBEBEB;
+    
+`
